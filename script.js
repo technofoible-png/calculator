b8b160c13138f9aa34bdf90d9ff2d58f34461a98
technofoible <techno.foible@gmail.com>
2021-05-04 20:02:38 -0500

    created simple operation functions

---
+++ script.js
@@ -0,0 +1,19 @@
+// Adds a to b
+function add (a, b) {
+    return a + b;
+}
+
+// Subtracts a from b
+function sub (a, b) {
+    return a - b;
+}
+
+// Multiplies a times b
+function times (a, b) {
+    return a * b;
+}
+
+// Divides a by b 
+function divides (a, b) {
+    return a / b;
+}
